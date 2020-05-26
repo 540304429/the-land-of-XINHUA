8f25636c23ba975b37aabb8c767c2183339fc3ad
123biubiu <49528072+123biubiu@users.noreply.github.com>
2020-05-26 22:24:22 +0800

    0.01Version of 角色
    上传角色材质，以及按空格朝着x方向运动。（后续会根据格子判断移动方向）。角色移动动画也上传了，但还没有安装上。明天再试试

---
+++ The land of Xinhua/Assets/Scripts/Scripts_Player_control/Movement_Control.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_Control : MonoBehaviour
+{
+
+    private BoxCollider coll;
+    private Rigidbody rb;
+    [Header("movement parameters")]
+    public float speed = 0.08f;
+
+    [Header("status")]
+    public bool isOnGround;
+    public bool isWalking;
+    Vector3 Xmovement = new Vector3(1, 0, 0);
+    Vector3 Ymovement = new Vector3(0, -1, 0);
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (Input.GetKey("space"))
+        {
+
+            rb.transform.position += Xmovement*speed;
+
+        }
+
+    }
+    void Update()
+    {
+       
+    }
+    
+}
