6f10655716d4407ba5257967cf81177d3406c2d3
123biubiu <49528072+123biubiu@users.noreply.github.com>
2020-05-27 23:03:41 +0800

    V0.02
    添加了走路动作。以及按键控制走路和停止走路。按空格前进，按s停止走路。明天完成控制转身，走路，以及根据格子走路。

--- The land of Xinhua/Assets/Scripts/Scripts_Player_control/Movement_Control.cs
+++ The land of Xinhua/Assets/Scripts/Scripts_Player_control/Movement_Control.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class Movement_Control : MonoBehaviour
 {
 
+    public Animator anim;
     private BoxCollider coll;
     private Rigidbody rb;
     [Header("movement parameters")]
-    public float speed = 0.08f;
+    public float speed = 8f;
 
     [Header("status")]
     public bool isOnGround;
     public bool isWalking;
     Vector3 Xmovement = new Vector3(1, 0, 0);
-    Vector3 Ymovement = new Vector3(0, -1, 0);
-
-
-
+    Vector3 Ymovement = new Vector3(0, 1, 0);
+    Vector3 Zmovement = new Vector3(0, 0, 1);
+    int direction;
+    float orginalZ;
+    //public int Power = 10;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -28,15 +31,40 @@ public class Movement_Control : MonoBehaviour
     {
         if (Input.GetKey("space"))
         {
-
-            rb.transform.position += Xmovement*speed;
-
+            
+            Walk();
+            
         }
-
+        if (Input.GetKey("d"))
+        {
+            StopWalking();
+        }
     }
     void Update()
     {
        
     }
-    
+    void Walk() {
+        //rb.transform.position += -Zmovement * speed;
+        //Vector3 direction = new Vector3(rb.velocity.x, rb.velocity.x, -speed * Time.deltaTime);
+        isWalking = true;
+        orginalZ = transform.position.z;
+        direction = -1;
+        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, direction * speed * Time.deltaTime);
+        //rb.AddForce(direction*Zmovement, ForceMode.Force);
+        anim.SetBool("Walk", true);
+        
+
+
+    }
+    void StopWalking() {
+        isOnGround = true;
+        rb.velocity = new Vector3(0, 0, 0);
+        anim.SetBool("Walk", false);
+
+    }
+
+
+
+
 }
