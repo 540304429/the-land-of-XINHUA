c35653c4e18ab62e5df3d1e8b2936d34d9a9ea8b
123biubiu <49528072+123biubiu@users.noreply.github.com>
2020-05-28 19:27:49 +0800

    fixed commit

---
+++ land/Assets/scripts/[liu] ui/GameManager1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager
+{
+    private static GameManager _Instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_Instance == null)
+            {
+                _Instance = new GameManager();
+
+
+            }
+            return _Instance;
+
+        }
+
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+
+}
